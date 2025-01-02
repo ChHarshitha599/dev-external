271a5abadb61770a0b8c5ee1ad9f254e50c0e914
ChHarshitha599 <147132208+ChHarshitha599@users.noreply.github.com>
2025-01-02 10:42:34 +0530

    Update numbers.java

--- numbers.java
+++ numbers.java
@@ -1,10 +1,9 @@
 package internal2;
-import java.util.*;
+
 public class numbers {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+		int n = 10;
 		int prod=1;
 		for (int i=1;i<=n;i++) {
 			prod*=i;
