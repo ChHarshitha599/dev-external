1b7d9a50b195860c2a60e2781309d80b70d9ed7e
ChHarshitha599 <147132208+ChHarshitha599@users.noreply.github.com>
2025-01-02 10:44:25 +0530

    Update numbers.java

--- numbers.java
+++ numbers.java
@@ -3,11 +3,11 @@ package internal2;
 public class numbers {
 
 	public static void main(String[] args) {
-		int n = 10;
+		int n = 3;
 		int prod=1;
 		for (int i=1;i<=n;i++) {
 			prod*=i;
 		}
-		System.out.println("factorial of n is"+prod);
+		System.out.println("factorial of n is"+" "+prod);
 	}
 }
